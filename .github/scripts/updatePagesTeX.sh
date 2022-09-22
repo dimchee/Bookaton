18d4369590c9ad9c6d6223fb97bd14a6ffc5bbcd
dimchee <dimchee90@gmail.com>
2022-09-22 19:21:02 +0200

    Fix build action

--- .github/scripts/updatePagesTeX.sh
+++ .github/scripts/updatePagesTeX.sh
@@ -1,5 +1,10 @@
-rm Src/Pages.tex
-for i in $(ls Src/Pages)
-do
-    printf '\input{Src/Pages/%s}\n' ${i%.*} >> Src/Pages.tex
-done
+if [ -d Src/Pages ]
+then
+    rm Src/Pages.tex
+    for i in $(ls Src/Pages)
+    do
+        printf '\input{Src/Pages/%s}\n' ${i%.*} >> Src/Pages.tex
+    done
+else
+    echo "No Src/Pages, Doing Nothing"
+fi 
