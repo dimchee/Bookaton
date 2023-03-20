7956d64e24e914926cdc7b589976cd475c9b8997
dimchee <41148612+dimchee@users.noreply.github.com>
2023-03-21 00:40:20 +0100

    Update updatePagesTeX.sh

--- .github/scripts/updatePagesTeX.sh
+++ .github/scripts/updatePagesTeX.sh
@@ -1,7 +1,7 @@
 if [ -d Src/Pages ]
 then
     rm Src/Pages.tex
-for i in $(ls Src/Pages/*.tex)
+for i in $(ls Src/Pages | grep tex$)
     do
         printf '\input{Pages/%s}\n' ${i%.*} >> Src/Pages.tex
     done
