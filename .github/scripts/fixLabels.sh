466b61e0996e2de0c88d9b777f4e1b07c49edf86
dimchee <dimchee90@gmail.com>
2023-03-22 20:42:02 +0100

    Add Revise issue Label

--- .github/scripts/fixLabels.sh
+++ .github/scripts/fixLabels.sh
@@ -2,5 +2,6 @@
 gh label list | awk -F '\t' '{ print $1 }' | xargs -I _ gh label delete --confirm '_'
 
 # Create labels for common things
-gh label create LaTeX --description "Potrebno je prekucati text u LaTeX" --color D93F0B
-gh label create Tikz  --description "Potrebno je pretvoriti sliku u LaTeX" --color E99695
+gh label create LaTeX  --description "Potrebno je prekucati text u LaTeX"  --color D93F0B
+gh label create Tikz   --description "Potrebno je pretvoriti sliku u Tikz" --color E99695
+gh label create Revise --description "Potrebno je označiti greške u PDFu"  --color 1D76DB
